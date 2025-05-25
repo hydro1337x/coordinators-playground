60c25d69beedc7fe7d42e4d2887e4f8e5005dac0
Benjamin Mecanović <48958924+hydro1337x@users.noreply.github.com>
2025-05-25 18:55:32 +0200

    Add screen-store matching test

---
+++ CoordinatorsPlaygroundTests/AuthCoordinatorFactoryTests.swift
@@ -0,0 +1,23 @@
+//
+//  AuthCoordinatorFactoryTests.swift
+//  CoordinatorsPlaygroundTests
+//
+//  Created by Benjamin Macanovic on 25.05.2025..
+//
+
+import Testing
+import SwiftUI
+@testable import CoordinatorsPlayground
+
+@MainActor
+struct AuthCoordinatorFactoryTests {
+    let sut = AccountCoordinatorFactory()
+    
+    @Test func test_makeAccountDetails() async throws {
+        let feature = sut.makeAccountDetails()
+        
+        #expect(feature.as(type: AccountDetailsStore.self) != nil)
+        #expect(String(describing: feature.underlyingFeature).contains("AccountDetailsScreen"))
+    }
+
+}
--- CoordinatorsPlaygroundTests/CoordinatorsPlaygroundTests.swift
+++ CoordinatorsPlaygroundTests/CoordinatorsPlaygroundTests.swift
@@ -149,10 +149,10 @@ struct CoordinatorsPlaygroundTests {
 //    }
     
 
-    @Test func example() async throws {
-        let url = URL(string: "coordinatorsplayground://deeplink?payload=W3sidmFsdWUiOiAidGFicyJ9LCB7InZhbHVlIjogImhvbWUifSwgeyJ2YWx1ZSI6ICJzY3JlZW5BIn0sIHsidmFsdWUiOiAic2NyZWVuQiIsICJwYXJhbWV0ZXJzIjogeyJpZCI6ICIxIn19LCB7InZhbHVlIjogInNjcmVlbkMifV0=")!
-        
-        #expect(DeepLinkParser.parse(url) == [.tabs, .home, .screenA, .screenB(id: 1), .screenC])
-    }
+//    @Test func example() async throws {
+//        let url = URL(string: "coordinatorsplayground://deeplink?payload=W3sidmFsdWUiOiAidGFicyJ9LCB7InZhbHVlIjogImhvbWUifSwgeyJ2YWx1ZSI6ICJzY3JlZW5BIn0sIHsidmFsdWUiOiAic2NyZWVuQiIsICJwYXJhbWV0ZXJzIjogeyJpZCI6ICIxIn19LCB7InZhbHVlIjogInNjcmVlbkMifV0=")!
+//        
+//        #expect(DeepLinkParser.parse(url) == [.tabs, .home, .screenA, .screenB(id: 1), .screenC])
+//    }
 
 }
